c618a9ca4a99cde7b5639929e90f31805ae78f03
katerynaostrietsova <eostrecova112@gmail.com>
2025-10-12 15:54:53 +0300

    Delete affinelab1/File.h

--- affinelab1/File.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "AffineTransformation.h"
-#include <vector>
-#include <string>
-
-std::vector<AffineTransform> readTransforms(const std::string& filename);
-void readStartPoint(const std::string& filename, int& n, Point& start);
-void writePoints(const std::string& filename, const std::vector<Point>& points);
